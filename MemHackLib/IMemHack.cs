e48dcf36318720da36d203fc3a146116c0e44c13
Victor Sandu <vyctor661@gmail.com>
2025-02-11 17:22:25 +0200

    wip

---
+++ MemHackLib/IMemHack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemHackLib;
+public interface IMemHack
+{
+    public const uint BufferSize = 1024;
+    public Type ValueType { get; set; }
+
+    public static List<nint> MemorySearch(uint processId, long desiredValue);
+
+    public static string WriteAddressValue(uint processId, nint targetPointer, long value);
+
+    public static List<nint> FilterPointers(uint processId, List<nint> pointers, long newValue);
+
+
+    public static List<(nint hWnd, string title, uint processId)> GetAllWindows();
+
+}
---
+++ MemHackLib/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemHackLib;
+
+public class Utils
+{
+    public static long BufferConvert(byte[] buffer, int offset, Type type) => type switch
+    {
+        var valueType when valueType == typeof(short) => BitConverter.ToInt16(buffer, offset),
+        var valueType when valueType == typeof(int) => BitConverter.ToInt32(buffer, offset),
+        var valueType when valueType == typeof(long) => BitConverter.ToInt64(buffer, offset),
+        _ => BitConverter.ToInt32(buffer, offset),
+    };
+
+}
